8eba44d10cd50452cad7b267865fe1d8a5c12344
Alex Komanov <akomanov@seatgeek.com>
2019-12-20 11:35:47 +0200

    Added class Map

---
+++ src/map/MapClass.java
@@ -0,0 +1,39 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Alex Komanov
+ */
+public class MapClass {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+
+        /*
+        System.out.println(map);
+
+
+        map.put(3, "Another value of number three");
+        System.out.println(map);
+
+        System.out.println(map.get(1));
+        System.out.println(map.get(10));
+
+
+         */
+
+        //---foreach for a map instance---
+
+        for(Map.Entry<Integer, String> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+
+
+    }
+}
---
+++ src/map/MapTypes.java
@@ -0,0 +1,39 @@
+package map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author Alex Komanov
+ */
+public class MapTypes {
+    public static void main(String[] args) {
+        Map<Integer, String> hashMap = new HashMap<>(); // No order of (key,value)
+
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>(); //Order by added objects
+
+        Map<Integer, String> treeMap = new TreeMap<>(); // Order by the key(natural order)
+
+        testMap(hashMap);
+        System.out.println("======================================");
+        testMap(linkedHashMap);
+        System.out.println("======================================");
+        testMap(treeMap);
+
+    }
+
+    public static void testMap(Map<Integer, String> map){
+        map.put(12, "454645645");
+        map.put(1500, "ertrewer");
+        map.put(0, "e4r4r4");
+        map.put(1, "cvcswwe");
+        map.put(1000, "zasxcdewq");
+        map.put(800, "454645645");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
